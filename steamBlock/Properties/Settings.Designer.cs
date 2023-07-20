27eaab4ab281377a2e21fd5f6bc18d1b532b0371
blardjosh <blard1337@gmail.com>
2023-07-19 20:51:18 -0700

    small changes and updated publishing setup

--- steamBlock/Properties/Settings.Designer.cs
+++ steamBlock/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace steamBlock.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("\"\"")]
+        [global::System.Configuration.DefaultSettingValueAttribute("@\"C:\\Program Files (x86)\\Steam\\steam.exe\"")]
         public string saveAppDir {
             get {
                 return ((string)(this["saveAppDir"]));
